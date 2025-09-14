e7454ac24bb027f0b8e45668e4d41a469c3e4225
CorranHornet <texhex14@outlook.com>
2025-09-14 15:09:22 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace inlämning_vecka3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
